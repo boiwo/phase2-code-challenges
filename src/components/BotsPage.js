75553438757f3b4560ed5037af71bffdd7cadaa7
benard <benardboiwo703@gmail.com>
2024-05-06 11:52:03 +0300

    add pot page

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -8,7 +8,7 @@ function BotsPage() {
 
   
   function fetchData() {
-    return fetch(`http://localhost:3000/bots`)
+    return fetch(`http://localhost:3004/bots`)
       .then((resp) => resp.json())
       .then((data) => {
         setBots(data);
